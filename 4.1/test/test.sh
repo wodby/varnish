14ed31242b8de55530e84973f9965be176f615e2
Chingis Sandanov <chingis@wodby.com>
2017-03-20 18:54:00 +0700

    Check response

--- 4.1/test/test.sh
+++ 4.1/test/test.sh
@@ -9,4 +9,5 @@ fi
 docker-compose up -d
 docker-compose exec varnish make check-ready -f /usr/local/bin/actions.mk
 docker-compose exec varnish make flush -f /usr/local/bin/actions.mk
+docker-compose exec varnish curl -s "localhost:6081" | grep -q 'Welcome to nginx!'
 docker-compose down
