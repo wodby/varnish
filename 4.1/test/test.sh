de6f9f1205aebd71b3ae2db1e8b3e7fcb227f3eb
Chingis Sandanov <chingis@wodby.com>
2017-03-20 18:49:18 +0700

    Cleanup tests

--- 4.1/test/test.sh
+++ 4.1/test/test.sh
@@ -9,4 +9,4 @@ fi
 docker-compose up -d
 docker-compose exec varnish make check-ready -f /usr/local/bin/actions.mk
 docker-compose exec varnish make flush -f /usr/local/bin/actions.mk
-#docker-compose down
+docker-compose down
