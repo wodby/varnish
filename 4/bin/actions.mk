8cea8f98534b790217b4280ecc10e71518a36c65
Chingis Sandanov <chingis@wodby.com>
2018-09-11 19:30:18 +0700

    Hard-code varnishadm port for orchestration actions

--- 4/bin/actions.mk
+++ 4/bin/actions.mk
@@ -10,7 +10,7 @@ service = Varnish
 default: check-ready
 
 flush:
-	varnishadm -T $(host) -S /etc/varnish/secret "ban req.http.host ~ ."
+	varnishadm -T $(host):6082 -S /etc/varnish/secret "ban req.http.host ~ ."
 
 check-ready:
 	wait_for "$(command)" $(service) $(host) $(max_try) $(wait_seconds) $(delay_seconds)
