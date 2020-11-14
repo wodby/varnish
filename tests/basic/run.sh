3136cf3cb8458a406f88541fc73b1f427f666601
Chingis Sandanov <chingis@wodby.com>
2020-11-15 03:56:36 +0700

    Fixes

--- tests/basic/run.sh
+++ tests/basic/run.sh
@@ -27,25 +27,25 @@ varnish curl -s -o /dev/null -w '%{http_code}' "localhost:6081/.vchealthz" | gre
 echo "OK"
 
 echo -n "Checking varnish backend response containing a country code header detected via geoip module... "
-docker-compose exec php sh -c 'echo "<?php var_dump(\$_SERVER[\"HTTP_X_COUNTRY_CODE\"]);" > /var/www/html/index.php'
+docker-compose exec -T php sh -c 'echo "<?php var_dump(\$_SERVER[\"HTTP_X_COUNTRY_CODE\"]);" > /var/www/html/index.php'
 varnish curl --header "X-Real-IP: ${us_ip}" -s "localhost:6081" | grep -q "US"
 varnish make flush -f /usr/local/bin/actions.mk
 echo "OK"
 
 echo -n "Checking varnish backend response containing the currency... "
-docker-compose exec php sh -c 'echo "<?php var_dump(\$_SERVER[\"HTTP_X_CURRENCY\"]);" > /var/www/html/index.php'
+docker-compose exec -T php sh -c 'echo "<?php var_dump(\$_SERVER[\"HTTP_X_CURRENCY\"]);" > /var/www/html/index.php'
 varnish curl --header "X-Real-IP: ${us_ip}" -s "localhost:6081" | grep -q "USD"
 varnish make flush -f /usr/local/bin/actions.mk
 echo "OK"
 
 echo -n "Checking varnish backend response containing the currency (from Cloudflare \"CF-IPCountry\" header)... "
-docker-compose exec php sh -c 'echo "<?php var_dump(\$_SERVER[\"HTTP_X_CURRENCY\"]);" > /var/www/html/index.php'
+docker-compose exec -T php sh -c 'echo "<?php var_dump(\$_SERVER[\"HTTP_X_CURRENCY\"]);" > /var/www/html/index.php'
 varnish curl --header "CF-IPCountry: US" -s "localhost:6081" | grep -q "USD"
 varnish make flush -f /usr/local/bin/actions.mk
 echo "OK"
 
 echo -n "Checking varnish VCKEY cookies... "
-docker-compose exec php sh -c 'echo "<?php echo(\"Hello World\");" > /var/www/html/index.php'
+docker-compose exec -T php sh -c 'echo "<?php echo(\"Hello World\");" > /var/www/html/index.php'
 varnish sh -c 'curl -sI -b "VCKEYinvalid=123"  localhost:6081 | grep -q "X-VC-Cache: MISS"'
 varnish sh -c 'curl -sI -b "VCKEYinvalid=123"  localhost:6081 | grep -q "X-VC-Cache: MISS"'
 varnish sh -c 'curl -sI -b "VCKEY-.invalid=123"  localhost:6081 | grep -q "X-VC-Cache: MISS"'
